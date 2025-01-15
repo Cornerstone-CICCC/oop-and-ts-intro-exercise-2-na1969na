caf476c4ab8ed8f6f3d84f119bd5d72a33d62adf
Elmer Balbin Jr. <contact@elmer.dev>
2025-01-15 05:58:35 -0800

    Update exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -2,8 +2,8 @@
 //
 // 1. Create a class `Animal` with a private property `#type` and a getter `getType` to access the private property.
 // 2. The `Animal` class should have a method `speak()` that returns "The [type] makes a sound" using the getter.
-// 3. Create a subclass `Dog` that overrides the `speak()` method to return "The dog barks", but still use the parent `getType` getter in the subclass.
-// 4. Create another subclass `Cat` that overrides the `speak()` method to return "The cat meows", but still use the parent `getType` getter in the subclass.
+// 3. Create a subclass `Dog` that overrides the `speak()` method to return "The dog barks". "dog" needs to pass as an argument to the parent constructor using `super`.
+// 4. Create another subclass `Cat` that overrides the `speak()` method to return "The cat meows". "cat" needs to pass as an argument to the parent constructor using `super`.
 
 
 class Animal {
